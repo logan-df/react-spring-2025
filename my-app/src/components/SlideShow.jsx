c82caf89ed6277ebe2762fa5c999f9d714f8c2df
logan-df <ldford711@gmail.com>
2025-04-01 09:44:04 -0400

    exercises

---
+++ my-app/src/components/SlideShow.jsx
@@ -0,0 +1,42 @@
+import "./css/SlideShow.css";
+import {useState} from "react";
+
+const SlideShow = () => {
+    const [imageIndex, setImageIndex] = useState(0);
+
+    const importAll = (resource) => {
+        return resource.keys().map(resource);
+    };
+
+    const images = importAll(
+        require.context("../images/slideshow", false, /\.(png|jpe?g|svg$|webp)/)
+    );
+
+    const ShowNextImage = () => {
+        /*
+        if(imageIndex == images.length - 1) {
+            setImageIndex(0);
+        }
+        setImageIndex(imageIndex+1);
+        */
+        setImageIndex(imageIndex == images.length -1? 0: imageIndex +1);
+    };
+
+    const showPreviousImage = () => {
+        setImageIndex(imageIndex == 0? images.length -1: imageIndex -1);
+    };
+
+    return (
+        <>
+            <section id="slideshow">
+                <img src={images[imageIndex]} />
+                    <p>
+                        <button onClick={showPreviousImage}>Previous</button>
+                        <button onClick={ShowNextImage}>Next</button>
+                    </p>
+                </section>
+        </>
+    );
+};
+
+export default SlideShow;
