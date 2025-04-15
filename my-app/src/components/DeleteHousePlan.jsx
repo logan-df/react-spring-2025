716be065db92bcafdc6f8efcc8c90303f4c220e8
logan-df <ldford711@gmail.com>
2025-04-15 09:36:50 -0400

    changes

---
+++ my-app/src/components/DeleteHousePlan.jsx
@@ -0,0 +1,46 @@
+import "./css/Dialog.css";
+import React, { useState } from "react";
+
+const DeleteHousePlan = (props) => {
+  const [result, setResult] = useState("");
+
+  const deleteHousePlan = async() => {
+    const response = await fetch(`http://localhost:3001/api/houses/${props._id}`,{
+      method:"DELETE"
+    });
+
+    if(response.status === 200){
+      setResult("House plan deleted successfully");
+      props.closeDeleteDialog();
+      props.hideHousePlan();
+    } else {
+      setResult("Sorry, we couldn't delete your house righ now");
+    }
+  };
+
+  return (
+    <div id="delete-dialog" className="w3-modal">
+      <div className="w3-modal-content">
+        <div className="w3-container">
+          <span
+            id="dialog-close"
+            className="w3-button w3-display-topright"
+            onClick = {props.closeDeleteDialog}
+          >
+            &times;
+          </span>
+          <div id="delete-content">
+            <h3>Are you sure you want to delete the {props.name}</h3>
+            <section>
+              <button onClick = {props.closeDeleteDialog}>No</button>            
+              <button onClick={deleteHousePlan}>Yes</button>
+            </section>
+            <span>{result}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteHousePlan
--- my-app/src/components/HousePlan.jsx
+++ my-app/src/components/HousePlan.jsx
@@ -1,19 +1,90 @@
 import "./css/HousePlan.css";
+import React, { useState } from "react";
+import EditHousePlan from "./EditHousePlan";
+import DeleteHousePlan from "./DeleteHousePlan";
 
 const HousePlan = (props) => {
-    return (
-        <section className="house-plan columns">
+  const [housePlan, setHousePlan] = useState(props);
+  const [showEditDialog, setShowEditDialog] = useState(false);
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [showHousePlan, setShowHousePlan] = useState(true);
+
+  const openEditDialog = () => {
+    setShowEditDialog(true);
+  };
+
+  const closeEditDialog = () => {
+    setShowEditDialog(false);
+  };
+
+  const openDeleteDialog = () => {
+    setShowDeleteDialog(true);
+  };
+
+  const closeDeleteDialog = () => {
+    setShowDeleteDialog(false);
+  };
+
+  const hideHousePlan = () => {
+    setShowHousePlan(false);
+  }
+
+  return (
+    <>
+      {showHousePlan?(
+        <div>
+            {showDeleteDialog?(
+              <DeleteHousePlan
+              name={housePlan.name}
+              _id={housePlan._id}
+              closeDeleteDialog = {closeDeleteDialog}
+              hideHousePlan = {hideHousePlan}
+            />
+            ):("")}
+            
+            {showEditDialog?(
+               <EditHousePlan
+               _id={housePlan._id}
+               name={housePlan.name}
+               size={housePlan.size}
+               main_image={housePlan.main_image}
+               bedrooms={housePlan.bedrooms}
+               bathrooms={housePlan.bathrooms}
+               features={housePlan.features}
+               closeEditDialog = {closeEditDialog}
+             />
+            ):("")}
+           
+
+          <section className="house-plan columns">
             <section className="feature-image">
-                <img src={"http://localhost:3001/images/" + props.main_image} alt={props.name}/>
+              <img
+                src={"http://localhost:3001/images/" + housePlan.main_image}
+                alt={housePlan.name}
+              />
             </section>
-            <section className = "info">
-                <h3>{props.name}</h3>
-                <p>Size: {props.size}</p>
-                <p>Bedrooms: {props.bedrooms}</p>
-                <p>Bathrooms: {props.bathrooms}</p>
+            <section className="info">
+              <header className="columns">
+                <h3>{housePlan.name}</h3>
+                <section id="change-buttons">
+                  <a href="#" onClick={openEditDialog} >
+                    &#9998;
+                  </a>
+                  <a href="#" onClick={openDeleteDialog}>
+                    &#x2715;
+                  </a>
+                </section>
+              </header>
+
+              <p>Size: {housePlan.size}</p>
+              <p>Bedrooms: {housePlan.bedrooms}</p>
+              <p>Bathrooms: {housePlan.bathrooms}</p>
             </section>
-        </section>
-    );
+          </section>
+        </div>
+      ):("")}
+    </>
+  );
 };
 
 export default HousePlan;
